1e17e2f243500480916d6c315321689c4ba4d8c8
MiaMarmeladenbrot <mia.mecklenburg@gmx.net>
2024-05-23 08:48:49 +0200

    edit tweet done

---
+++ backend/README.md
@@ -0,0 +1,4 @@
+# Todos
+
+- deleteUser: auch alle seine Tweets und Likes sowie reTweets von ihm anderer User löschen -> seine Id aus allen Referenzen entfernen
+- editTweet: soll man retweetTweetId hinterher bearbeiten können?
--- backend/src/services/tweetServices/editTweet.js
+++ backend/src/services/tweetServices/editTweet.js
@@ -1,43 +1,24 @@
 import { Tweet } from "../../models/Tweet.js";
 
 export async function editTweet(tweetId, updateTweetInfo) {
-  // prüfen, ob tweet existiert
-  // prüfen, ob message <= 160
-  // message und retweetedTweetId prüfen => Längen bzw. existiert retweetedTweetId?
-  // tweet updaten
-  // return updated tweet
-
   const tweet = await Tweet.findById(tweetId);
   if (!tweet) throw new Error("Tweet not found.");
 
-  if (updateTweetInfo.message && updateTweetInfo.message.length > 160)
+  // falls eine message geändert werden soll, darf sie nur max 160 Zeichen lang sein
+  if (updateTweetInfo.message?.length > 160)
     throw new Error("Tweets cannot exceed a length of 160 characters.");
 
-  //   if (!updateTweetInfo.message && !updateTweetInfo.retweetedTweetId)
-  //     throw new Error("Tweets need content - what do you wanna say?");
+  // falls eine message geändert werden soll, darf sie nicht leer sein
+  if (updateTweetInfo.message?.length < 1)
+    throw new Error("Tweets need content - what do you wanna say?");
 
-  //   # morgen noch mal fresh!
-  //   if (
-  //     updateTweetInfo.message &&
-  //     updateTweetInfo.message.length === 0 &&
-  //     updateTweetInfo.retweetedTweetId &&
-  //     updateTweetInfo.retweetedTweetId.length === 0
-  //   )
-  //     throw new Error("Tweets need content - what do you wanna say?");
-
-  // wir wollen verhindern, dass User den Content seines Tweets leeren kann, also das Folgendes passiert:
-  //    "message": "",
-  //    "retweetedTweetId": null,
-
-  if (updateTweetInfo.message && updateTweetInfo.message.length === 0)
-    throw new Error("Tweets need content - what do you wanna say?"); //--> warum wird "" nicht als 0 gezählt? mit === 1 funktioniert die Abfrage ...
-
-  if (
-    updateTweetInfo.retweetedTweetId &&
-    updateTweetInfo.retweetedTweetId.length === 0
-    // außerdem: existiert dieser Tweet überhaupt?
-  )
-    throw new Error("Tweets need content - what do you wanna say?"); // --> bekommt eh error: "Cast to ObjectId failed for value \"\" (type string) at path \"retweetedTweetId\" because of \"BSONError\""
+  // falls man retweetTweetId bearbeiten kann, brauchen wir abfragen - wollen wir im Frontend aber einfach nicht zulassen
+  // // falls eine Referenz zu einem anderen Tweet (retweet) hinzugefügt werden soll, darf das nicht leer sein
+  // if (
+  //   updateTweetInfo.retweetedTweetId?.length < 1
+  //   // außerdem: existiert dieser Tweet überhaupt?
+  // )
+  //   throw new Error("Tweets need content - what do you wanna say?"); // --> bekommt eh error: "Cast to ObjectId failed for value \"\" (type string) at path \"retweetedTweetId\" because of \"BSONError\""
 
   const updatedTweet = await Tweet.findByIdAndUpdate(
     tweetId,

a48eb3d4bb31c151d59a044353a5d26fa2188189
MiaMarmeladenbrot <mia.mecklenburg@gmx.net>
2024-05-23 12:59:23 +0200

    fix addTweet

--- backend/src/services/tweetServices/addTweet.js
+++ backend/src/services/tweetServices/addTweet.js
@@ -1,24 +1,21 @@
-import { Tweet } from "../../models/Tweet.js"
+import { Tweet } from "../../models/Tweet.js";
 
 export async function addTweet(
   authenticatedUserId,
-  { userId, message, retweetedTweetId, isLikedBy }
+  { message, retweetedTweetId, isLikedBy }
 ) {
-  // # Enkomplizieren
-  const useridString = userId.toString()
-  if (authenticatedUserId !== useridString)
-    throw new Error("You are not authorized to post this tweet.")
+  if (message.length > 160)
+    throw new Error("Tweets cannot exceed a length of 160 characters.");
 
-  if (message.length > 160) throw new Error("Tweets cannot exceed a length of 160 characters.")
-
-  if (!message && !retweetedTweetId) throw new Error("Tweets need content - what do you wanna say?")
+  if (!message && !retweetedTweetId)
+    throw new Error("Tweets need content - what do you wanna say?");
 
   const newTweet = await Tweet.create({
     userId: authenticatedUserId,
     message,
     retweetedTweetId,
     isLikedBy,
-  })
+  });
 
-  return { newTweet }
+  return { newTweet };
 }

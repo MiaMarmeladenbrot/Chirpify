5cb7310d0a7aafbfb16c6cc879e036ad550d4c24
MiaMarmeladenbrot <mia.mecklenburg@gmx.net>
2024-05-24 15:54:30 +0200

    addTweetButton, FooterNav, HeaderNav, starting FeedPage

--- frontend/src/components/HeaderNav/HeaderNav.jsx
+++ frontend/src/components/HeaderNav/HeaderNav.jsx
@@ -2,6 +2,8 @@ import "./HeaderNav.css";
 import { useLocation, useNavigate } from "react-router-dom";
 import ImageBird from "../ImageBird/ImageBird";
 import { FaArrowLeft } from "react-icons/fa6";
+import ImageProfile from "../ImageProfile/ImageProfile";
+import { IoIosSettings } from "react-icons/io";
 
 const HeaderNav = () => {
   const { pathname } = useLocation();
@@ -11,16 +13,29 @@ const HeaderNav = () => {
     return navigate("/landing");
   };
 
+  const refreshFeed = () => {
+    navigate("/feed");
+    window.scrollTo(0, 0);
+  };
+
   return (
     <header>
-      <nav className="headerNav">
+      <nav className={pathname === "/feed" ? "headerNav-big" : "headerNav"}>
         {pathname === "/register" && (
           <FaArrowLeft className="arrow-position" onClick={goBack} />
         )}
         {pathname === "/login" && (
           <FaArrowLeft className="arrow-position" onClick={goBack} />
         )}
-        <ImageBird />
+        {pathname === "/feed" && <ImageProfile />}
+        <ImageBird onClick={pathname === "/feed" ? refreshFeed : ""} />
+        {pathname === "/feed" && (
+          <IoIosSettings
+            color="#1D9BF0"
+            size={60}
+            onClick={() => navigate("/settings")}
+          />
+        )}
       </nav>
     </header>
   );
--- frontend/src/pages/FeedPage/FeedPage.jsx
+++ frontend/src/pages/FeedPage/FeedPage.jsx
@@ -1,12 +1,39 @@
+import { useContext, useEffect, useState } from "react";
+import FooterNav from "../../components/FooterNav/FooterNav";
+import HeaderNav from "../../components/HeaderNav/HeaderNav";
 import TweetAddButton from "../../components/TweetAddButton/TweetAddButton";
+import { backendUrl } from "../../api/api";
+import { accessTokenContext } from "../../context/Context";
+
+// # hier den allUsers-Fetch global?
 
 const FeedPage = () => {
+  const [userFeed, setUserFeed] = useState("");
+  const { accessToken } = useContext(accessTokenContext);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(`${backendUrl}/api/v1/tweets/userFeed`, {
+        headers: { authorization: `Bearer ${accessToken}` },
+      });
+
+      const data = await res.json();
+      // if (!data.result) {
+      //   return setErrorMessage(data.message);
+      // }
+      setUserFeed(data);
+      console.log(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <section>
-      <div>
-        <h1>feedpage</h1>
-        <TweetAddButton />
-      </div>
+      <HeaderNav />
+
+      <TweetAddButton />
+      <FooterNav />
     </section>
   );
 };

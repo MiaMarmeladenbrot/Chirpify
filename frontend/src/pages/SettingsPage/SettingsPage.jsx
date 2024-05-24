bb07b301f4ed547ed209a81f45f6cd5c5948c0f0
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-05-24 14:51:14 +0200

    Add SettingsPage

--- frontend/src/pages/SettingsPage/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage/SettingsPage.jsx
@@ -1,5 +1,69 @@
+import "./SettingsPage.css"
+import { useContext } from "react"
+import { userContext } from "../../context/Context"
+import { Link } from "react-router-dom"
+import { MdOutlineKeyboardArrowRight } from "react-icons/md"
+
 const SettingsPage = () => {
-  return <div>SettingsPage</div>
+  const { user } = useContext(userContext)
+
+  console.log(user)
+
+  return (
+    <main className="settings">
+      <article className="settings__header">
+        <h1>Settings and privacy</h1>
+        <div className="settings__header__link">
+          <Link to="feed">Done</Link>
+        </div>
+      </article>
+      <article className="settings__container">
+        <h2>@{user.username}</h2>
+        <div>
+          <div className="settings__link-container disabled-link">
+            <Link>Account</Link>
+            <MdOutlineKeyboardArrowRight />
+          </div>
+          <div className="settings__link-container">
+            <Link to="/settings/emailverification">Email Verification</Link>
+            <MdOutlineKeyboardArrowRight />
+          </div>
+          <div className="settings__link-container disabled-link">
+            <Link>Privacy and safety</Link>
+            <MdOutlineKeyboardArrowRight />
+          </div>
+          <div className="settings__link-container disabled-link">
+            <Link>Content Preferences</Link>
+            <MdOutlineKeyboardArrowRight />
+          </div>
+        </div>
+      </article>
+      <article className="settings__container">
+        <h2>General</h2>
+        <div>
+          <div className="settings__link-container disabled-link">
+            <Link>Display and sound</Link>
+            <MdOutlineKeyboardArrowRight />
+          </div>
+          <div className="settings__link-container disabled-link">
+            <Link>Data usage</Link>
+            <MdOutlineKeyboardArrowRight />
+          </div>
+          <div className="settings__link-container disabled-link">
+            <Link>Accessability</Link>
+            <MdOutlineKeyboardArrowRight />
+          </div>
+          <div className="settings__link-container disabled-link">
+            <Link>About Twitter</Link>
+            <MdOutlineKeyboardArrowRight />
+          </div>
+        </div>
+      </article>
+      <article className="settings__information">
+        <p>General settings affect all of your Twitter accounts on this device.</p>
+      </article>
+    </main>
+  )
 }
 
 export default SettingsPage

6f9347f8da9f40e8202248940ecadad6c394dcd9
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-05-24 10:38:41 +0200

    Add Landingpage

--- frontend/src/components/HeaderNav/HeaderNav.jsx
+++ frontend/src/components/HeaderNav/HeaderNav.jsx
@@ -1,9 +1,25 @@
+import "./HeaderNav.css"
+import { useLocation } from "react-router-dom"
+import ImageBird from "../ImageBird/ImageBird"
+import { FaArrowLeft } from "react-icons/fa6"
+
 // Nav to Profil
 // Nav to Settings
 // Bird to Feed Top of the Page
+// ! Conditional Rendering depending on the location
+// ! CSS muss wahrscheinlich auch Conditional für headerNav je nach Componentent
 
 const HeaderNav = () => {
-  return <div>HeaderNav</div>
+  const { pathname } = useLocation()
+
+  return (
+    <header>
+      <nav className="headerNav">
+        {pathname === "/register" && <FaArrowLeft />}
+        <ImageBird />
+      </nav>
+    </header>
+  )
 }
 
 export default HeaderNav
--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,27 @@
+import HeaderNav from "../../components/HeaderNav/HeaderNav"
+import "./LandingPage.css"
+import { Link } from "react-router-dom"
+
 const LandingPage = () => {
-  return <div>LandingPage</div>
+  return (
+    <>
+      <HeaderNav />
+      <main className="landingPage">
+        <h1>Welcome to Chirpify</h1>
+        <h2>Find out what's going on in the world right now.</h2>
+        <div className="landingPage__link">
+          <Link to="/register">Create account</Link>
+        </div>
+        <p>By signing up, you agree to our Termns, Privacy Policy and Cookie Use.</p>
+        <div className="landingPage__Login-Container">
+          <p>Have an account already?</p>
+          <div className="landingPage__link--login">
+            <Link to="/login">Log in</Link>
+          </div>
+        </div>
+      </main>
+    </>
+  )
 }
 
 export default LandingPage

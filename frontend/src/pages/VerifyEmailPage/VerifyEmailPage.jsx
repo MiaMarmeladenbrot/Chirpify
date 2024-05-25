bbd991ddab9242679c51acc287c711d6184826a1
MiaMarmeladenbrot <mia.mecklenburg@gmx.net>
2024-05-25 10:13:10 +0200

    fix navigate loading and sendEmail

--- frontend/src/pages/VerifyEmailPage/VerifyEmailPage.jsx
+++ frontend/src/pages/VerifyEmailPage/VerifyEmailPage.jsx
@@ -1,64 +1,71 @@
-import { useContext, useState } from "react"
-import "./VerifyEmailPage.css"
-import { accessTokenContext, userContext } from "../../context/Context"
-import { backendUrl } from "../../api/api"
-import { useNavigate } from "react-router-dom"
+import { useContext, useState } from "react";
+import "./VerifyEmailPage.css";
+import { accessTokenContext, userContext } from "../../context/Context";
+import { backendUrl } from "../../api/api";
+import { useNavigate } from "react-router-dom";
+import FooterNav from "../../components/FooterNav/FooterNav";
 
 // # Footer aufnehmen
 
 const VerifyEmailPage = () => {
-  const { user, setUser } = useContext(userContext)
-  const { accessToken } = useContext(accessTokenContext)
-  const [sixDigitCode, setSixDigitCode] = useState("")
-  const navigate = useNavigate()
-  const [error, setError] = useState("")
+  const { user, setUser } = useContext(userContext);
+  const { accessToken } = useContext(accessTokenContext);
+  const [sixDigitCode, setSixDigitCode] = useState("");
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     const res = await fetch(`${backendUrl}/api/v1/users/verifyEmail`, {
       method: "POST",
-      headers: { "Content-Type": "application/json", authorization: `Bearer ${accessToken}` },
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${accessToken}`,
+      },
       body: JSON.stringify({ sixDigitCode }),
-    })
+    });
 
-    const data = await res.json()
+    const data = await res.json();
 
     if (!data.result?.isEmailVerified) {
-      setError("error")
-      setSixDigitCode("")
-      return
+      setError("error");
+      setSixDigitCode("");
+      return;
     }
 
-    setUser(data.result)
-    navigate("/feed")
-    setSixDigitCode("")
-    setError("")
-  }
+    setUser(data.result);
+    navigate("/loading");
+    setSixDigitCode("");
+    setError("");
+  };
 
   const getNewSixDigitCode = async () => {
     const res = await fetch(`${backendUrl}/api/v1/users/sendVerifyEmail`, {
       method: "POST",
       headers: { authorization: `Bearer ${accessToken}` },
-    })
+    });
 
-    await res.json()
+    await res.json();
 
-    setDisableButton(true)
+    setDisableButton(true);
 
     const disableTimeout = setTimeout(() => {
-      setDisableButton(false)
-    }, 5000)
+      setDisableButton(false);
+    }, 5000);
 
-    return () => clearTimeout(disableTimeout)
-  }
+    return () => clearTimeout(disableTimeout);
+  };
 
   return (
     <main className="verifyemail">
       <h1>Verify your E-Mail</h1>
       <div className="verifyemail__input-container">
         <label htmlFor="sixDigitCode">Enter your 6-Digit-Code here</label>
-        <form onSubmit={handleSubmit} className="verifyemail__input-container__inputs">
+        <form
+          onSubmit={handleSubmit}
+          className="verifyemail__input-container__inputs"
+        >
           <input
             value={sixDigitCode}
             onChange={(e) => setSixDigitCode(e.target.value)}
@@ -69,17 +76,22 @@ const VerifyEmailPage = () => {
           />
           <button type="submit">Send</button>
         </form>
-        {error === "error" && <p className="errorMessage">Wrong 6-Digit-Code. Please Try again</p>}
+        {error === "error" && (
+          <p className="errorMessage">Wrong 6-Digit-Code. Please Try again</p>
+        )}
       </div>
 
       {user?.isEmailVerified === false && (
         <div className="verifyemail__input__new-email-container">
-          <p>Don't know your 6-digit code? We'll send you a new code by email.</p>
+          <p>
+            Don't know your 6-digit code? We'll send you a new code by email.
+          </p>
           <button onClick={getNewSixDigitCode}>New 6-Digit-Code</button>
         </div>
       )}
+      <FooterNav />
     </main>
-  )
-}
+  );
+};
 
-export default VerifyEmailPage
+export default VerifyEmailPage;

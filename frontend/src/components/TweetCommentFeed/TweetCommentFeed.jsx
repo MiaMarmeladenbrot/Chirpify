676a69a84c098f13cad130bcdf992a8bd6db5c5b
MiaMarmeladenbrot <mia.mecklenburg@gmx.net>
2024-05-29 14:53:52 +0200

    add edit comp to comments

--- frontend/src/components/TweetCommentFeed/TweetCommentFeed.jsx
+++ frontend/src/components/TweetCommentFeed/TweetCommentFeed.jsx
@@ -1,16 +1,23 @@
 import { useContext, useEffect, useState } from "react";
 import "./TweetCommentFeed.css";
 import { backendUrl } from "../../api/api";
-import { accessTokenContext } from "../../context/Context";
+import { accessTokenContext, userContext } from "../../context/Context";
 import TweetAddComment from "../TweetAddComment/TweetAddComment";
+import IconDelete from "../IconDelete/IconDelete";
+import IconEdit from "../IconEdit/IconEdit";
 
 const TweetCommentFeed = ({
   singleTweet,
   rerenderCounter,
   setRerenderCounter,
 }) => {
+  const { user: loggedInUser } = useContext(userContext);
+  console.log(loggedInUser);
   const { accessToken } = useContext(accessTokenContext);
   const [comments, setComments] = useState(null);
+  const [message, setMessage] = useState("");
+  const [toggleEdit, setToggleEdit] = useState(false);
+
   const [errorMessage, setErrorMessage] = useState(null);
   const tweetId = singleTweet?._id;
   const user = singleTweet?.userId;
@@ -22,16 +29,15 @@ const TweetCommentFeed = ({
       });
 
       const data = await res.json();
-      console.log(data);
       if (!data.result) {
         return setErrorMessage(data.message);
       }
       setComments(data.result);
-      console.log(comments);
       setErrorMessage("");
     };
     fetchAllCommentsOfTweet();
   }, [rerenderCounter]);
+  console.log(comments);
 
   // createdAt : "2024-05-28T14:00:11.453Z"
   // message : "genau so"
@@ -48,36 +54,68 @@ const TweetCommentFeed = ({
   // _id: "6655e36bcf8970ce433d0970"
 
   return (
-    <>
-      {comments?.length !== 0 ? (
-        <div className="tweet-comment-feed">
-          {comments?.map((singleComment) => (
-            <div key={singleComment._id} className="single-comment">
-              <div className="comment-profile-area">
-                <img
-                  src={`${backendUrl}/${singleComment?.userId?.profileImg}`}
-                  alt={singleComment?.userId?.username}
-                />
-                <p>{singleComment?.userId?.firstname}</p>
-                <p>{singleComment?.userId?.lastname}</p>
-                <p>@{singleComment?.userId?.username}</p>
+    <section className="comments">
+      <article className="comments-with-input">
+        {comments?.length !== 0 ? (
+          <div className="tweet-comment-feed">
+            {comments?.map((singleComment) => (
+              <div key={singleComment._id} className="single-comment">
+                <div className="comment-profile-area">
+                  <img
+                    src={`${backendUrl}/${singleComment?.userId?.profileImg}`}
+                    alt={singleComment?.userId?.username}
+                  />
+                  <p>{singleComment?.userId?.firstname}</p>
+                  <p>{singleComment?.userId?.lastname}</p>
+                  <p>@{singleComment?.userId?.username}</p>
+                </div>
+
+                {singleComment?.userId?._id === loggedInUser._id ? (
+                  <div className="comment-menu">
+                    <IconDelete
+                      singleTweet={singleTweet}
+                      setErrorMessage={setErrorMessage}
+                    />
+                    <IconEdit
+                      singleComment={singleComment}
+                      message={message}
+                      setMessage={setMessage}
+                      setErrorMessage={setErrorMessage}
+                      toggleEdit={toggleEdit}
+                      setToggleEdit={setToggleEdit}
+                    />
+                  </div>
+                ) : (
+                  "fehler"
+                )}
+
+                {toggleEdit ? (
+                  <textarea
+                    name="edit-comment"
+                    id="edit-comment"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                  >
+                    {message}
+                  </textarea>
+                ) : (
+                  <p>{singleComment?.message}</p>
+                )}
               </div>
-              <p>{singleComment?.message}</p>
-            </div>
-          ))}
-          {/* hier input feld, um selbst einen Kommentar hinzuzufügen - dabei rerenderCount setten */}
-        </div>
-      ) : (
-        <div className="tweet-comment-feed">
-          <p>No comments yet</p>
-        </div>
-      )}
-      <TweetAddComment
-        tweetId={tweetId}
-        rerenderCounter={rerenderCounter}
-        setRerenderCounter={setRerenderCounter}
-      />
-    </>
+            ))}
+          </div>
+        ) : (
+          <div className="tweet-comment-feed">
+            <p>No comments yet</p>
+          </div>
+        )}
+        <TweetAddComment
+          tweetId={tweetId}
+          rerenderCounter={rerenderCounter}
+          setRerenderCounter={setRerenderCounter}
+        />
+      </article>
+    </section>
   );
 };
 

c8f63982a7faaab858b3af871eb61a650823d88d
MiaMarmeladenbrot <mia.mecklenburg@gmx.net>
2024-06-14 08:53:39 +0200

    auth req for edit and delete button

--- frontend/src/components/AuthRequiredVerified/AuthRequiredVerified.jsx
+++ frontend/src/components/AuthRequiredVerified/AuthRequiredVerified.jsx
@@ -1,17 +1,22 @@
 import { useContext, useState } from "react";
 import { userContext } from "../../context/Context";
 import VerificationPopup from "../VerificationPopup/VerificationPopup";
-import { Navigate, useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
-const AuthRequiredVerified = ({ children }) => {
-  const { user, setUser } = useContext(userContext);
+const AuthRequiredVerified = ({ children, setToggleEdit, setShowDelete }) => {
+  const { user } = useContext(userContext);
   const [showPopup, setShowPopup] = useState(true);
 
   return (
     <div>
       {user && user.isEmailVerified && children}
       {user && user.isEmailVerified === false && (
-        <VerificationPopup showPopup={showPopup} setShowPopup={setShowPopup} />
+        <VerificationPopup
+          showPopup={showPopup}
+          setShowPopup={setShowPopup}
+          setToggleEdit={setToggleEdit}
+          setShowDelete={setShowDelete}
+        />
       )}
       {!user && <Navigate to="/login" />}
     </div>
--- frontend/src/components/IconDelete/IconDelete.jsx
+++ frontend/src/components/IconDelete/IconDelete.jsx
@@ -5,6 +5,7 @@ import {
   rerenderCounterContext,
 } from "../../context/Context";
 import "./IconDelete.css";
+import AuthRequiredVerified from "../AuthRequiredVerified/AuthRequiredVerified";
 
 const IconDelete = ({ singleTweet, singleComment, setErrorMessage }) => {
   const { accessToken } = useContext(accessTokenContext);
@@ -37,6 +38,8 @@ const IconDelete = ({ singleTweet, singleComment, setErrorMessage }) => {
     setErrorMessage("");
   };
 
+  console.log(showDelete);
+
   return (
     <div>
       {/* trash can icon */}
@@ -51,32 +54,34 @@ const IconDelete = ({ singleTweet, singleComment, setErrorMessage }) => {
       </svg>
 
       {showDelete && (
-        <section className="delete-tweet-popup">
-          <img src="/img/birdLogo.png" alt="bird logo" />
-          <p>Are you sure you want to delete this?</p>
+        <AuthRequiredVerified setShowDelete={setShowDelete}>
+          <section className="delete-tweet-popup">
+            <img src="/img/birdLogo.png" alt="bird logo" />
+            <p>Are you sure you want to delete this?</p>
 
-          {/* check-icon */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-            width={18}
-            fill="#4C9BF0"
-            onClick={deleteTweetOrComment}
-          >
-            <path d="M64 80c-8.8 0-16 7.2-16 16V416c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16V96c0-8.8-7.2-16-16-16H64zM0 96C0 60.7 28.7 32 64 32H384c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96zM337 209L209 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L303 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" />
-          </svg>
+            {/* check-icon */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+              width={18}
+              fill="#4C9BF0"
+              onClick={deleteTweetOrComment}
+            >
+              <path d="M64 80c-8.8 0-16 7.2-16 16V416c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16V96c0-8.8-7.2-16-16-16H64zM0 96C0 60.7 28.7 32 64 32H384c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96zM337 209L209 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L303 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" />
+            </svg>
 
-          {/* x-icon */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-            width={20}
-            fill="#ce395f"
-            onClick={() => setShowDelete(false)}
-          >
-            <path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z" />
-          </svg>
-        </section>
+            {/* x-icon */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              width={20}
+              fill="#ce395f"
+              onClick={() => setShowDelete(false)}
+            >
+              <path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z" />
+            </svg>
+          </section>
+        </AuthRequiredVerified>
       )}
     </div>
   );
